616282efcf205d65100017ba9785ab29e8830f7a
Yithis <kula.jedrek@gmail.com>
2020-01-14 23:50:12 +0100

    Create a function to round image into an ellipse with given width and height

--- imagepipeline/src/main/java/com/facebook/imagepipeline/filter/InPlaceEllipseRoundFilter.java
+++ imagepipeline/src/main/java/com/facebook/imagepipeline/filter/InPlaceEllipseRoundFilter.java
@@ -41,5 +41,45 @@ public final class InPlaceEllipseRoundFilter {
 
         bitmap.setPixels(pixels, 0, w, 0, 0, w, h);
     }
+
+    /**
+     * An implementation for rounding a given bitmap to an ellipse shape with given width and height.
+     *
+     * @param bitmap The input {@link Bitmap}
+     * @param h Height of ellipse
+     * @param w Width of ellipse
+     */
+    public static void roundEllipseBitmapInPlace(Bitmap bitmap, int h, int w) {
+        Preconditions.checkNotNull(bitmap);
+        final int imageWidth = bitmap.getWidth();
+        final int imageHeight = bitmap.getHeight();
+
+        Preconditions.checkArgument(w <= imageWidth);
+        Preconditions.checkArgument(h <= imageHeight);
+
+        final int[] transparentColor = new int[imageWidth];
+
+        final int[] pixels = new int[imageWidth * imageHeight];
+        bitmap.getPixels(pixels, 0, imageWidth, 0, 0, imageWidth, imageHeight);
+
+        for (int y = -1*imageHeight/2; y < imageHeight/2; y++) {
+            int x = -1*imageWidth/2;
+            while ( ((float) 4*x*x)/(w*w) + ((float) 4*y*y)/(h*h) > 1 && x < 0 ) {
+                x++;
+            }
+            if (x < 0) {
+                int pixelsToHide = Math.max(0, x + imageWidth/2 - 1);
+
+                System.arraycopy(transparentColor, 0, pixels, (y + imageHeight/2)*imageWidth, pixelsToHide);
+
+                System.arraycopy(transparentColor, 0, pixels, (y + imageHeight/2)*imageWidth + imageWidth - pixelsToHide, pixelsToHide);
+            }
+            else {
+                System.arraycopy(transparentColor, 0, pixels, (y + imageHeight / 2) * imageWidth, imageWidth);
+            }
+        }
+
+        bitmap.setPixels(pixels, 0, imageWidth, 0, 0, imageWidth, imageHeight);
+    }
 }
 
